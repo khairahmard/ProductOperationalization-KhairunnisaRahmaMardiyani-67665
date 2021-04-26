cc1908c382a0b725212de427b06c4a80b439cb33
khairahmard <khairunnisamardiyani@gmail.com>
2021-04-26 20:48:37 +0700

    add coment

--- main.py
+++ main.py
@@ -17,16 +17,19 @@ df['release_year'] = df['releaseDate'].str[:4].astype('int64')
 def read_root():
 	return data
 
+#menampilkan jumlah trakcs setiap unit
 @app.get("/countUnitSong")
 def read_countUnitSong():
 	api = df.groupby(['artistName'])[['trackName']].count().rename(columns={'trackName':'count'}).sort_values(['count'], ascending=False)
 	return api.to_dict(orient='dict')
 
+#menampilkan 5 album dengan jumlah lagu terbanyak
 @app.get("/albumTracks")
 def read_countUnitSong():
 	api = result_2 = df.groupby(['collectionName'])[['trackName']].count().rename(columns={'trackName':'count'}).sort_values(['count'], ascending=False).head(5)
 	return api.to_dict(orient='dict')
 
+#menampilkan 5 rata-rata tertinggi harga album
 @app.get("/albumPrice")
 def read_countUnitSong():
 	api = df.groupby(['artistName'])[['collectionPrice']].mean().rename(columns={'collectionPrice':'mean'}).sort_values(['mean'], ascending=False).head(5)
