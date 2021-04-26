453710b1930b47a73464d4e23cc1f846cd37d3e3
khairahmard <khairunnisamardiyani@gmail.com>
2021-04-26 20:44:45 +0700

    upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+import json
+import pandas as pd
+from urllib.request import urlopen
+
+app = FastAPI()
+
+data = []
+
+url = 'https://itunes.apple.com/search?term=NCT&limit=200'
+res = urlopen(url)
+data.extend(json.load(res)['results'])
+df = pd.json_normalize(data)
+df['release_year'] = df['releaseDate'].str[:4].astype('int64')
+
+@app.get("/")
+def read_root():
+	return data
+
+@app.get("/countUnitSong")
+def read_countUnitSong():
+	api = df.groupby(['artistName'])[['trackName']].count().rename(columns={'trackName':'count'}).sort_values(['count'], ascending=False)
+	return api.to_dict(orient='dict')
+
+@app.get("/albumTracks")
+def read_countUnitSong():
+	api = result_2 = df.groupby(['collectionName'])[['trackName']].count().rename(columns={'trackName':'count'}).sort_values(['count'], ascending=False).head(5)
+	return api.to_dict(orient='dict')
+
+@app.get("/albumPrice")
+def read_countUnitSong():
+	api = df.groupby(['artistName'])[['collectionPrice']].mean().rename(columns={'collectionPrice':'mean'}).sort_values(['mean'], ascending=False).head(5)
+	return api.to_dict(orient='dict')
+
